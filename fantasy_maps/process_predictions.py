42e791efac20950bcce3d7ed0ca3d93e6074fa79
Eric Schmidt <erschmid@google.com>
2021-09-16 22:08:18 +0000

    feat: updates to process_predictions

--- fantasy_maps/process_predictions.py
+++ fantasy_maps/process_predictions.py
@@ -1,28 +1,30 @@
 from typing import Tuple
 
-def process_predictions(predictions) -> Tuple:
-  """Extracts IDs, confidences, display names, and bounding boxes from Vertex AI predictions.
+def process_predictions(predictions, is_printed_to_out=False) -> Tuple:
+    """Extracts IDs, confidences, display names, bounding boxes from first VertexAI prediction
 
-  Args:
+    Args:
     predictions: a list of Vertex AI ImageObjectPredictionResult objects
-  
-  Returns:
-    Tuple(bounding boxes, confidences, IDs, display names)
-  """
-  bboxes = None
 
-  for num_prediction, prediction_ in enumerate(predictions):
+    Returns:
+    Tuple(bounding boxes, confidences, IDs, display names)
+    """
+    if (len(predictions) == 0):
+        return
+
+    prediction_ = predictions[0]
+
     # ids, confidences, displayNames, bboxes
-    print(f"Prediction number: {num_prediction + 1}\n")
     ids = prediction_["ids"]
     confidences = prediction_["confidences"]
     display_names = prediction_["displayNames"]
     bboxes = prediction_["bboxes"]
 
-    for count, id in enumerate(ids):
-      print(f"ID: {id}")
-      print(f"Display name: {display_names[count]}")
-      print(f"Confidence: {confidences[count]}")
-      print(f"Bounding boxes: {bboxes[count]}\n\n")
+    if is_printed_to_out:
+        for count, id in enumerate(ids):
+            print(f"ID: {id}")
+            print(f"Display name: {display_names[count]}")
+            print(f"Confidence: {confidences[count]}")
+            print(f"Bounding boxes: {bboxes[count]}\n\n")
   
-  return (bboxes, confidences, ids, display_names)
+    return (bboxes, confidences, ids, display_names)
